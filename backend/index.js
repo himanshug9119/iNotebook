03930a14f3739d61960394c5f9949dc662bd110f
Himanshu Gupta <guptasitapur489@gmail.com>
2024-03-31 14:47:48 +0530

    Connected to MongoDB

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const db = require("./db");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Himanshu!");
+}
+);
+// listen 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
